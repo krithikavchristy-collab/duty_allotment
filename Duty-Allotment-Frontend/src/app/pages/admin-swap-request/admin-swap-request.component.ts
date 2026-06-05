3e1625402060b51b43829a669089decba401c092
libishalibisha30-hue <libishalibisha30@gmail.com>
2026-06-05 10:28:11 +0530

    completed duty history

--- Duty-Allotment-Frontend/src/app/pages/admin-swap-request/admin-swap-request.component.ts
+++ Duty-Allotment-Frontend/src/app/pages/admin-swap-request/admin-swap-request.component.ts
@@ -7,4 +7,37 @@ import { Component } from '@angular/core';
 })
 export class AdminSwapRequestComponent {
 
+  swapRequests = [
+    {
+      requester: {
+        name: 'Bonnie Green',
+        image: 'assets/images/image1.jpg',
+        duty: 'Exam Duty - 22 May 2026',
+        time: '9:00 AM - 12:00 PM',
+        block: 'Block A 101',
+        reason: 'I have a meeting',
+        requestedOn: '18 May 2026'
+      },
+      requestedWith: {
+        name: 'John',
+        image: 'assets/images/image2.jpg'
+      }
+    },
+    {
+      requester: {
+        name: 'Bonnie Green',
+        image: 'assets/images/image1.jpg',
+        duty: 'Exam Duty - 22 May 2026',
+        time: '9:00 AM - 12:00 PM',
+        block: 'Block A 101',
+        reason: 'I have a meeting',
+        requestedOn: '18 May 2026'
+      },
+      requestedWith: {
+        name: 'John',
+        image: 'assets/images/image2.jpg'
+      }
+    }
+  ];
+
 }
