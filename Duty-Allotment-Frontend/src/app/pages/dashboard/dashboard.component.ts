cab36a62be9882a42c9b038116355d14d4a31da0
libishalibisha30-hue <libishalibisha30@gmail.com>
2026-06-05 12:25:03 +0530

    completed duty history

--- Duty-Allotment-Frontend/src/app/pages/dashboard/dashboard.component.ts
+++ Duty-Allotment-Frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
+  activeTab: string = 'dashboard';
 
+  setTab(tab: string) {
+    this.activeTab = tab;
+  }
 }
